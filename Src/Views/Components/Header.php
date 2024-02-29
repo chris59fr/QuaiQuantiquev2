826ec12ccbc1e8492286e63e2417ac6c87837c93
Justine Garandel <jg.garandel@hotmail.fr>
2024-02-29 14:15:32 +0100

    update scss

---
+++ Src/Views/Components/Header.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <link rel="stylesheet" href="../../../Public/Assets/CSS/bootstrap.min.css">
+  <link rel="stylesheet" href="../../../Public/Assets/CSS/main.css">
+</head>
+<body>
+  <header>
+    <div class="line-containerUp">
+      <div class="lineUp"></div>
+    </div>
+      
+      <?php require_once './NavBar.php';?>
+
+    <div class="line-containerDown">
+      <div class="lineDown"></div>
+    </div>
+  </header>
+
+  <script src="../../../Public/Assets/JS/bootstrap.bundle.min.js"></script>
+</body>
+</html>
+
+
+
