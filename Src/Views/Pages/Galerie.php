c9869e134750bbf5cd87db1d5567daa04ced6b7b
youssouf doucoure <madkdouc@yahoo.com>
2024-03-07 13:05:29 +0100

    add branch galerie

--- Src/Views/Pages/Galerie.php
+++ Src/Views/Pages/Galerie.php
@@ -6,35 +6,16 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-    <link rel="stylesheet" href="./galerie.css">
-    <link href="https://fonts.googleapis.com/css2?family=Luxurious+Script&display=swap" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css2?family=Jost:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;1,300&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="../../../Public/Assets/CSS/main.css">
     <script src="https://kit.fontawesome.com/d3e5723829.js" crossorigin="anonymous"></script>
     <title>QuaiQantique</title>
 </head>
 
+
+<?php  require_once '../Components/Header.php';
+?>
+
 <header class="container-fluid">
-    <div class="collapse" id="navbarToggleExternalContent">
-        <div class="bg p-4">
-            <h5 class="text-body-emphasis h4">Bienvenue</h5>
-            <span class="text-body-secondary">Dans notre univers de la Haute-Savoie</span>
-        </div>
-    </div>
-
-    <nav class="navbar navbar ">
-        <button>
-            <span class="navbar-toggler-icon"></span>
-        </button>
-            <div class="slogan">
-                <h1>Galerie</h1>
-            </div>
-            <div class="home">
-                <img src="/Back/public/assets/Logo/house-solid.svg">
-            </div>    
-        </div>
-    
-    </nav>
-
     <div class="menus">
         <nav>
             <li>
@@ -61,67 +42,10 @@
 
 </main>
 
+<?php
+    require_once '../Components/Footer.php';
+?>
 
-<footer class="container">
-
-    <div class="ouverture">
-        <div class="page">
-            <h2>Horaires d'ouvertures</h2>
-        </div>
-        <div class="horaire">
-            <div class="jour">
-                <p>Lundi</p>
-                <p>Mardi</p>
-                <p>Mercredi</p>
-                <p>Jeudi</p>
-                <P>Vendredi</P>
-                <p>Samedi</p>
-                <P>Dimanche</P>
-            </div>
-
-            <div class="heure">
-                <p>12:00-15:00 / 19:00-22:00</p>
-                <p>12:00-15:00 / 19:00-22:00</p>
-                <p>Fermé</p>
-                <p>12:00-15:00 / 19:00-22:00</p>
-                <p>12:00-15:00 / 19:00-22:00</p>
-                <p>18:00-23:00</p>
-                <p>11:00-14:00</p>
-            </div>
-
-        </div>
-        <h2>Nos Adresses</h2>
-        <div class=" adress">
-            <h4>PARIS</h4>
-            <p>11 rue du Quai</p>
-            <p>75016 Paris</p>
-            <p>Tél: +33 (0)1 02 03 04 05</p>
-
-            <hr>
-
-            <h4>CHAMBERY</h4>
-            <P>1529 Rte Saint-Saturnin</P>
-            <p>73018 chambéry</p>
-            <p>Tél: +33 (0)1 06 07 08 09</p>
-        </div>
-        <div class="contact">
-            <button>Contact</button>
-        </div>
-        <div class="signature">
-            <img src="/QuaiQuantiquev2/Public/Assets/Images/Logo/SignatureSize3.svg">
-            <h3><span>Quai</span> antique</h3>
-            <p>Au plaisir de vous recevoir !</p>
-        </div>
-
-
-        <div class="trait"> </div>
-        <div class="copyrigth">
-            <p> Copyright 2024 - Restaurant Chambéry - QUAI ANTIQUE</p>
-            <p>Mention légale - Politique de confidentialité</p>
-        </div>
-    </div>
-
-</footer>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"crossorigin="anonymous"></script>
 </body>
 
