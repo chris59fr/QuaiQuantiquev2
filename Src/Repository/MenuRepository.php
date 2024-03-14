330a2de0ee3645208e3474c89bd75ca720730661
Justine Garandel <jg.garandel@hotmail.fr>
2024-03-14 19:42:26 +0100

    add file Abstract and MenuRepository

---
+++ Src/Repository/MenuRepository.php
@@ -0,0 +1,25 @@
+<?php
+namespace Src\Repository;
+
+use Src\Repository\abstractRepository;
+use PDO;
+use PDOException;
+
+class MenuRepository extends abstractRepository {
+  
+  /**
+   * Récupération de tous les menus de la BDD
+   */
+  public function getAll() {
+    try {
+      $requetes = $this->getDBConnection()->prepare("SELECT * FROM `menu`");
+      $requetes->execute();
+
+      return $requetes->fetchAll(PDO::FETCH_ASSOC);
+    } catch (PDOException $error)
+    {
+      echo 'Erreur de récupération :' . $error->getMessage();
+    }
+    
+  }
+}
