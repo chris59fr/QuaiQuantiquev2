80ed988ca4d2ab07e095e38f3ba6db4b8631ad0b
Justine Garandel <jg.garandel@hotmail.fr>
2024-03-14 20:33:40 +0100

    Update .php

--- Src/Views/Pages/Home.php
+++ Src/Views/Pages/Home.php
@@ -1,13 +1,14 @@
 <!DOCTYPE html>
 <html lang="fr">
+
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Home</title>
   <link rel="stylesheet" href="../../../Public/Assets/CSS/bootstrap.css">
-  <link rel="stylesheet" href="../../../Public/Assets/CSS/main.css">
-  
+  <link rel="stylesheet" href="../../../Scss/main.css">
 </head>
+
 <body>
 
   <?php require_once '../Components/Header.php'; ?>
@@ -143,4 +144,5 @@
 
   <script src="../../../Public/Assets/JS/bootstrap.bundle.min.js"></script>
 </body>
+
 </html>
