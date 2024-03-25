4c74159e9783fa103e34c37ee0add6842f40d189
Ruyssen Romain <ruyssensud@gmail.com>
2024-03-25 18:54:57 +0100

    not important
    save for just switch in dev branch

--- Config/Exception/RouteurException.php
+++ Config/Exception/RouteurException.php
@@ -1,8 +1,10 @@
 <?php
 
 namespace App\config\Exception;
-
+use App\Models\ErrorModel;
     class RouteurException extends \Exception
     {
-           
+        public function __construct($code = 0, $message = "") {
+            parent::__construct(ErrorModel::getErrorMessage($code, $message), $code);
+        }
     }
--- Src/Models/ErrorModel.php
+++ Src/Models/ErrorModel.php
@@ -4,7 +4,7 @@ namespace App\Models;
 class ErrorModel {
     public static function getErrorMessage($code, $customMessage = "") {
         $httpErrorMessage = match ($code) {
-            404 => 'La page demandée n’a pas été trouvée.',
+            404 => 'La page demandée n’a pas été trouvée.', // si message d erreur sur vs code normal php 8 avec fonction match pas encore reconnu 
             500 => 'Erreur interne du serveur.',
             default => 'Une erreur inattendue est survenue.',
         };
